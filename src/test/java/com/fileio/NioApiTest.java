27ce5cb9142c4d514dec4f1e8f3d5522adbb5b50
ShradhaNbr <sradha.nambiar@gmail.com>
2021-08-06 17:11:51 +0530

    Added: Code to watch particular directory

--- src/test/java/com/fileio/NioApiTest.java
+++ src/test/java/com/fileio/NioApiTest.java
@@ -49,4 +49,11 @@ public class NioApiTest {
         Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
                 .forEach(System.out::println);
     }
+
+    @Test
+    public void givenDirectoryWhenWatchedListsAllTheActivities () throws IOException {
+        Path dir =Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new WatchServices(dir).processEvents();
+    }
 }
